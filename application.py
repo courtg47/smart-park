18b05e69f379cbb8f05501aaea3b864debf4f76e
Courtney Green <cgreen@Courtneys-MacBook-Pro.local>
2018-08-18 13:24:21 -0500

    Added basic Flask application

--- application.py
+++ application.py
@@ -3,3 +3,23 @@
 """
 This is the main python application for the Smart Park project.
 """
+
+# Importing Flask
+from flask import (
+    Flask,
+    jsonify,
+    render_template,
+    request,
+    url_for
+)
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """ Render the homepage. """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
