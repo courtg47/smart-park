0524c3c13d5444a5de316fe14d42030aa1ccd218
Courtney Green <cgreen@Courtneys-MacBook-Pro.local>
2018-08-18 22:02:45 -0500

    Feature: create Javascript clock

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,31 @@
 // JavaScript
+$(document).ready(function() {
+
+  function clock() {
+    const date = new Date();
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let halfOfDay = "AM";
+
+    // Converts military time to standard American time with AM and PM.
+    if (hours == 0) {
+      hours = 12;
+    } else if (hours > 12) {
+      hours = hours - 12;
+      halfOfDay = "PM";
+    }
+
+    // Adds a 0 to the beginning of minutes and seconds
+    minutes < 10 ? minutes = "0" + minutes : minutes;
+    seconds < 10 ? seconds = "0" + seconds : seconds;
+
+    // Actual time that will be appended to the screen
+    const time = hours + ":" + minutes + ":" + seconds + " " + halfOfDay;
+
+    $(".second-box").html(time);
+  }
+
+  setInterval(clock, 1000);
+
+});
