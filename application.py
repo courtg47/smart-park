d8af5fbb1ea4ceb8b954bee3a102bc91645b2956
Katie Ford <katherine.ford@dreamhost.com>
2018-08-18 20:06:13 -0700

    pass instagram url to template

--- application.py
+++ application.py
@@ -13,17 +13,17 @@ from flask import (
     url_for
 )
 
-from helpers import timepractice
+from helpers import timepractice, instagram
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     """ Render the homepage. """
-    # bulletin = helpers.bulletin
     the_gif = timepractice.gif_picker()
+    insta = instagram.get_recent_instagram()
 
-    return render_template('index.html', gif=the_gif)
+    return render_template('index.html', gif=the_gif, instagram=insta)
 
 if __name__ == '__main__':
     app.debug = True
